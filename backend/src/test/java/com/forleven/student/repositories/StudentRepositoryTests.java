116791cf1db178c2d77c9b71d175350a1a1c45f5
Dyego Lima <dyegodll18@gmail.com>
2023-08-30 13:45:30 -0300

    Update StudentRepositoryTests

--- backend/src/test/java/com/forleven/student/repositories/StudentRepositoryTests.java
+++ backend/src/test/java/com/forleven/student/repositories/StudentRepositoryTests.java
@@ -1,6 +1,5 @@
 package com.forleven.student.repositories;
 
-	
 import java.util.Optional;
 
 import org.junit.jupiter.api.Assertions;
@@ -11,72 +10,89 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import com.forleven.student.entities.Student;
 
-	@DataJpaTest
-	class StudentRepositoryTests {
+@DataJpaTest
+class StudentRepositoryTests {
 
-		@Autowired
-		private StudentRepository repository;
+	@Autowired
+	private StudentRepository repository;
 
-		private Long existingId;
-		private Long inexistingId;
-		
-		@BeforeEach
-		void setUp() throws Exception{
-			existingId = 1L;
-			inexistingId = 100L;
-		}
-		
-		@Test
-		void findByIdShouldDoNothingWhenIdDoesNotExist() {
-			Assertions.assertDoesNotThrow(()->{
-				repository.findById(inexistingId);
-			});
-		}
-		
-		@Test
-		void findByIdShouldFindByIdObjectWhenIdExists() {
-			Optional<Student> obj = repository.findById(existingId);
-			Assertions.assertTrue(!obj.isEmpty());
-		}
-		
-		@Test
-		void insertShouldInsertObjectWhenEnrollmentsStudentDoesNotExist() {
-			Student entity = new Student("Enroll_0100", "Jhon", "Zane");
-			Assertions.assertNull(repository.findByEnrollment(entity.getEnrollment()));
-			entity = repository.save(entity);
-		}
-		
-		@Test
-		void insertShouldInsertObjectWhenGetClassThenEqualsStudent() {
-			Student entity = new Student("Enroll_01", "Alex", "Brown");
-			Assertions.assertSame(Student.class, entity.getClass());
-			entity = repository.save(entity);
-		}
-		
-		@Test
-		void updateShouldDoNothingWhenIdDoesNotExist() {
-			Assertions.assertDoesNotThrow(()->{
-				repository.getReferenceById(inexistingId);
-			});
-		}
-		
-		@Test
-		void updateShouldUpdateObjectWhenIdExists() {
-			Assertions.assertNotNull(repository.getReferenceById(existingId));
-		}
+	private Long existingId;
+	private Long inexistingId;
 
-		@Test
-		void deleteShouldDeleteObjectWhenIdExists() {
-			repository.deleteById(existingId);
-			Optional<Student> obj = repository.findById(existingId);
-			Assertions.assertTrue(obj.isEmpty());
-		}
-		
-		@Test
-		void deleteShouldDoNothingWhenIdDoesNotExist() {
-			Assertions.assertDoesNotThrow(()->{
-				repository.deleteById(inexistingId);
-			});
-		}
+	@BeforeEach
+	void setUp() throws Exception {
+		existingId = 1L;
+		inexistingId = 100L;
+	}
+
+	@Test
+	void findByIdShouldDoNothingWhenIdDoesNotExist() {
+		Assertions.assertDoesNotThrow(() -> {
+			repository.findById(inexistingId);
+		});
+	}
+
+	@Test
+	void findByIdShouldFindByIdObjectWhenIdExists() {
+		Optional<Student> obj = repository.findById(existingId);
+		Assertions.assertTrue(obj.isPresent());
+		;
+	}
+
+	@Test
+	void insertShouldInsertStudentWithIdAutoincrementWhenIdIsNull() {
+		Student entity = new Student("Enroll_100", "Jhon", "Zane");
+		entity.setId(null);
+		entity = repository.save(entity);
+		Assertions.assertNotNull(entity.getId());
+		Assertions.assertEquals(repository.count() + 1, entity.getId());
+	}
+
+	@Test
+	void insertShouldInsertStudentWhenEnrollmentsHaveMoreThanThreeCharactersAndDoesNotExist() {
+		Student entity = new Student("Enroll_100", "Jhon", "Zane");
+		entity = repository.save(entity);
+		Assertions.assertEquals("Enroll_100", entity.getEnrollment());
+	}
+
+	@Test
+	void insertShouldStudentWhenFirstNameHaveMoreThanThreeCharacters() {
+		Student entity = new Student("Enroll_100", "Jhon", "Zane");
+		entity = repository.save(entity);
+		Assertions.assertEquals("Jhon", entity.getFirstName());
+	}
+
+	@Test
+	void insertShouldStudentWhenLastNameHaveMoreThanThreeCharacters() {
+		Student entity = new Student("Enroll_100", "Jhon", "Zane");
+		entity = repository.save(entity);
+		Assertions.assertEquals("Zane", entity.getLastName());
+	}
+
+	@Test
+	void updateShouldDoNothingWhenIdDoesNotExist() {
+		Assertions.assertDoesNotThrow(() -> {
+			repository.getReferenceById(inexistingId);
+		});
+	}
+
+	@Test
+	void updateShouldUpdateObjectWhenIdExists() {
+		Assertions.assertNotNull(repository.getReferenceById(existingId));
+	}
+
+	@Test
+	void deleteShouldDeleteObjectWhenIdExists() {
+		repository.deleteById(existingId);
+		Optional<Student> obj = repository.findById(existingId);
+		Assertions.assertTrue(obj.isEmpty());
+	}
+
+	@Test
+	void deleteShouldDoNothingWhenIdDoesNotExist() {
+		Assertions.assertDoesNotThrow(() -> {
+			repository.deleteById(inexistingId);
+		});
+	}
 
 }
